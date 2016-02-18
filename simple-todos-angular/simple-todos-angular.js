c8c0c8cf5bb55c17c3d23368d7fff669b709462d
Rion Gull <riongull@gmail.com>
2016-02-18 14:56:46 -0700

    Modified html, css, and js files so user can add todo items to the list and database right from the view/template.

--- simple-todos-angular/simple-todos-angular.js
+++ simple-todos-angular/simple-todos-angular.js
@@ -1,23 +1,19 @@
+Tasks = new Mongo.Collection('tasks');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  // This code only runs on the client
+  angular.module('simple-todos',['angular-meteor']);
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
+   function ($scope, $meteor) {
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+     $scope.tasks = $meteor.collection(Tasks);
+     $scope.addTask = function (newTask) {
+        $scope.tasks.push( {
+          text: newTask,
+          createdAt: new Date() }
+        );
+      };
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+   }]);
 }
